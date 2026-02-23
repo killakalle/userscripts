b88a94b9c3173a687517736d264d234c1ae254fd
killakalle <allesschlucker@gmx.net>
2026-02-23 22:57:08 +0100

    Update version to 0.3 and improve code formatting in theCrag Dashboard script

--- theCrag/Dashboard - Add areas to route tick items.js
+++ theCrag/Dashboard - Add areas to route tick items.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         theCrag - Dashboard - Add areas to route tick items
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Adds a route's area to tick items in the stream
 // @author       killakalle
 // @match        https://www.thecrag.com/
@@ -10,35 +10,38 @@
 // require       https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @require 	 https://greasyfork.org/scripts/31940-waitforkeyelements/code/waitForKeyElements.js?version=209282
 // @grant        none
+// @license      MIT
+// @downloadURL  https://greasyfork.org/en/scripts/429706-thecrag-dashboard-add-areas-to-route-tick-items
+// @updateURL    https://greasyfork.org/en/scripts/429706-thecrag-dashboard-add-areas-to-route-tick-items
 // ==/UserScript==
 
 waitForKeyElements('.tick-item', addAreaToRoute)
 
 // Returns the last area from a title string
-function getLastArea(title) {
-	const separator = '›'
-	const lastIndex = title.lastIndexOf(separator)
-	return title.substring(lastIndex + 1)
+function getLastArea (title) {
+  const separator = '›'
+  const lastIndex = title.lastIndexOf(separator)
+  return title.substring(lastIndex + 1)
 }
 
-function addAreaToRoute(jNode) {
-	console.log('theCrag - Adding area to route')
+function addAreaToRoute (jNode) {
+  console.log('theCrag - Adding area to route')
 
-	let route = jNode.find('.route')
-	let routeLink = route.find('a')
-	const routeLinkTitle = routeLink.attr('title')
-	const routeLinkText = routeLink.text()
-	const lastArea = getLastArea(routeLinkTitle)
-	let stars = 0
-	if (routeLinkText.lastIndexOf('★') >= 0) {
-		stars = routeLinkText.lastIndexOf('★') - routeLinkText.indexOf('★') + 1
-	}
-	routeLink.empty()
-	routeLink.append(document.createTextNode(lastArea + '› '))
-	for (let i = 0; i < stars; i++) {
-		let starSpan = document.createElement('span')
-		starSpan.classList.add('star')
-		routeLink.append(starSpan)
-	}
-	routeLink.append(document.createTextNode(routeLinkText.replaceAll('★', '')))
+  let route = jNode.find('.route')
+  let routeLink = route.find('a')
+  const routeLinkTitle = routeLink.attr('title')
+  const routeLinkText = routeLink.text()
+  const lastArea = getLastArea(routeLinkTitle)
+  let stars = 0
+  if (routeLinkText.lastIndexOf('★') >= 0) {
+    stars = routeLinkText.lastIndexOf('★') - routeLinkText.indexOf('★') + 1
+  }
+  routeLink.empty()
+  routeLink.append(document.createTextNode(lastArea + '› '))
+  for (let i = 0; i < stars; i++) {
+    let starSpan = document.createElement('span')
+    starSpan.classList.add('star')
+    routeLink.append(starSpan)
+  }
+  routeLink.append(document.createTextNode(routeLinkText.replaceAll('★', '')))
 }
