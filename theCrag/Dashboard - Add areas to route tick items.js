9260a28b9a3775d29350a891ee7ed165d19461d6
killakalle <allesschlucker@gmx.net>
2026-02-24 00:14:32 +0100

    Bump version to 0.7 for theCrag Dashboard user script

--- theCrag/Dashboard - Add areas to route tick items.js
+++ theCrag/Dashboard - Add areas to route tick items.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         theCrag - Dashboard - Add areas to route tick items
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Adds a route's area to tick items in the stream
 // @author       killakalle
 // @match        https://www.thecrag.com/
